53b1d9f7f50a2881da2f9a23db4bfc633c231a80
양지원 <rennes91@jobkorea.co.kr>
2024-04-04 17:09:34 +0900

    api 명세

--- src/main/java/com/template/app/controller/MemberController.java
+++ src/main/java/com/template/app/controller/MemberController.java
@@ -1,9 +1,5 @@
 package com.template.app.controller;
 
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,29 +9,22 @@ import com.template.app.model.request.UpdateMemberReq;
 import com.template.app.model.response.MemberInfoRes;
 import com.template.app.service.MemberAppService;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 
-@Validated
 @RestController
 @RequiredArgsConstructor
-public class MemberController {
+public class MemberController implements MemberControllerApi{
 	private final MemberAppService memberAppService;
 
-	@GetMapping("/member")
-	public MemberInfoRes getMember(@Valid @NotNull @Min(1) @RequestParam Integer memberNo){
+	public MemberInfoRes getMember(@RequestParam Integer memberNo){
 		return memberAppService.getMember(memberNo);
 	}
 
-	@PostMapping("/member")
-	public void saveMember(@RequestBody @Valid CreateMemberReq req){
+	public void saveMember(@RequestBody CreateMemberReq req){
 		memberAppService.saveMember(req);
 	}
 
-	@PutMapping("/member")
-	public void updateMember(@RequestBody @Valid UpdateMemberReq req){
+	public void updateMember(@RequestBody UpdateMemberReq req){
 		memberAppService.updateMember(req);
 	}
 }
---
+++ src/main/java/com/template/app/controller/MemberControllerApi.java
@@ -0,0 +1,64 @@
+package com.template.app.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.template.app.model.request.CreateMemberReq;
+import com.template.app.model.request.UpdateMemberReq;
+import com.template.app.model.response.MemberInfoRes;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+@Validated
+@Tag(name = "멤버", description = "멤버 API")
+public interface MemberControllerApi {
+	@Operation(
+			summary = "멤버 조회",
+			description = "멤버를 조회하는 API",
+			responses = {
+					@ApiResponse(description = "Success",
+							responseCode = "200",
+							content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = MemberInfoRes.class)))
+			}
+	)
+	@GetMapping("/member")
+	MemberInfoRes getMember(@Parameter(description = "멤버 넘버", example = "1", name = "memberNo")
+							@RequestParam @Valid @NotNull @Min(1) Integer memberNo);
+
+	@Operation(
+			summary = "멤버 등록",
+			description = "멤버를 등록하는 API",
+			responses = {
+					@ApiResponse(description = "Success",
+							responseCode = "200",
+							content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema()))
+			}
+	)
+	@PostMapping(value = "/member")
+	void saveMember(@RequestBody @Valid CreateMemberReq req);
+
+	@Operation(
+			summary = "멤버 수정",
+			description = "멤버를 수정하는 API",
+			responses = {
+					@ApiResponse(description = "Success",
+							responseCode = "200",
+							content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema()))
+			}
+	)
+	@PutMapping("/member")
+	void updateMember(@RequestBody @Valid UpdateMemberReq req);
+}
